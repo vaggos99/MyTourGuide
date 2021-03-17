2084e2c7095cbca7861f2017edfff02cbf7b1bad
Vaggelis Koronis <korwnis718@gmail.com>
2021-03-17 19:05:42 +0200

    create bottom navigation bar

--- app/src/main/java/com/unipi/p17050/mytourguide/MainActivity.java
+++ app/src/main/java/com/unipi/p17050/mytourguide/MainActivity.java
@@ -2,6 +2,7 @@ package com.unipi.p17050.mytourguide;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,17 +17,22 @@ import android.widget.TextView;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private SharedPreferences sharedPreferences;
+    private BottomNavigationView bnv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        bnv=findViewById(R.id.bottom_navigation);
+        bnv.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment()).commit();
     }
 
     @Override
@@ -63,6 +69,27 @@ public class MainActivity extends AppCompatActivity {
         return true;
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            switch(item.getItemId()){
+                case R.id.profile:
+                    selectedFragment=new ProfileFragment();
+                    break;
+                case R.id.start:
+                    selectedFragment=new GuideFragment();
+                    break;
+                case R.id.map:
+                    selectedFragment=new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
     private void logout(){
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
