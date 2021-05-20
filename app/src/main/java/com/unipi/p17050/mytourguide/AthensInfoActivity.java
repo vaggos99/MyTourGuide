24dae0025ad0b57efd45213500c331a07bf37241
Vaggelis Koronis <korwnis718@gmail.com>
2021-05-20 16:46:54 +0300

    create the athens info activity

--- app/src/main/java/com/unipi/p17050/mytourguide/AthensInfoActivity.java
+++ app/src/main/java/com/unipi/p17050/mytourguide/AthensInfoActivity.java
@@ -3,6 +3,9 @@ package com.unipi.p17050.mytourguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class AthensInfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class AthensInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_athens_info);
+        String htmlText = "<h2>"+getString(R.string.info_about_athens_header)+"</h2><br>\n" + "<p>"+getString(R.string.athens_info) +"</p><br>" + "<p>"+getString(R.string.athens_info1) +"</p><br>"
+                +"<p>"+getString(R.string.athens_info2)+"</p>";
+        TextView textView= findViewById(R.id.athens_infoTextView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setText(Html.fromHtml(htmlText, Html.FROM_HTML_MODE_COMPACT));
     }
 }
